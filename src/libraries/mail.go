33b710add80a4c0f51348d4147f7e688e60abb7f
Blake <tk0wo3@gmail.com>
2019-12-27 00:32:27 +0800

    add: email

--- src/libraries/mail.go
+++ src/libraries/mail.go
@@ -2,34 +2,71 @@ package libraries
 
 import (
     "bytes"
-    "fmt"
     "html/template"
+    "log"
     "net/smtp"
 )
 
-type EmailContent struct {
+type EmailRequest struct {
     Title string
-    Content string
+    content string
     To string
-    Views string
+    Subject string
+    View string
+    Data interface{}
+}
+//signature
+var account = "xxxxxx"
+
+var password = "xxxxxx"
+
+var auth smtp.Auth
+
+func (request *EmailRequest) GoogleEmailSend() {
+
+    auth = smtp.PlainAuth("", account, password, "smtp.gmail.com")
+    if err := request.parseTemplate(request.View, request.Data); err == nil {
+        result, _ := request.sendEmail()
+        log.Println(result)
+    } else {
+        log.Println(err)
+    }
 }
 
-func GoogleEmailSend(setting EmailContent) {
-    username := "#########"
-    password := "#########"
-    gmailAuth := smtp.PlainAuth("", username, password, "smtp.gmail.com")
-    temp, _ := template.ParseFiles(setting.Views)
+func (request *EmailRequest) parseTemplate(templateFile string, data interface{}) error {
+    var err error
+    view, err := template.ParseFiles(templateFile)
+    if err != nil {
+        return err
+    }
+    buf := new(bytes.Buffer)
+    if err = view.Execute(buf, data); err != nil {
+        return err
+    }
+    request.content = buf.String()
+    return nil
+}
 
-    var body bytes.Buffer
-    var header = "MIME-version: 1.0;\nContent-type: text/html;"
-    body.Write([]byte(fmt.Sprintf("Subject:%s\n%s\n\n", setting.Title, header)))
+func (request *EmailRequest) sendEmail() (bool, error) {
+    log.Println(request.Subject)
+    mime := "Message-ID: "+MakeMd5(MakeSnowId().String())+"\nReply-To: "+account+"\nFrom: Journeyfoxx <"+account+">"+"\nMIME-version: 1.0;\nTo: "+request.To+"\nContent-Type: text/html; charset=\"UTF-8\";\n"
+    subject := "Subject: " + request.Subject + "\n"
+    msg := []byte(subject + mime + "\n" + request.content)
+    addr := "smtp.gmail.com:587"
 
-    var content = struct{ token string }{
-        token: setting.Content,
+    if err := smtp.SendMail(addr, auth, account, []string{request.To}, msg); err != nil {
+        log.Println(err.Error())
+        return false, err
     }
 
-    _ = temp.Execute(&body, content)
-    var err error
-    err = smtp.SendMail("smtp.gmail.com:587", gmailAuth, username, []string{setting.To}, body.Bytes())
-    fmt.Print(err)
+    return true, nil
+}
+
+func EmailNewRequest(to, title, view string, data interface{}) *EmailRequest {
+    return &EmailRequest{
+        To: to,
+        Subject: title,
+        View: view,
+        Data: data,
+    }
 }
